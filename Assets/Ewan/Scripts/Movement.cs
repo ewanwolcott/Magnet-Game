c38efd69accc986a58216f70ad89ca3761906f29
MUKILTEO\1726637 <1726637@mukilteo.wednet.edu>
2025-11-18 10:17:42 -0800

    Player Movement

---
+++ Assets/Ewan/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [Header("Player Component Refrences")]
+    [SerializeField] Rigidbody2D rb;
+
+    [Header("Player Settings")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpingPower;
+
+    [Header("Grounding")]
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
+
+    private float horizontal;
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    #region PLAYER_CONTROLS
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+    }
+
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+
+
+    #endregion
+}
