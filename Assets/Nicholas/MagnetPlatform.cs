273fcf9c96e197d84d22e15990c268639ed9b417
Arcturus <1725310@mukilteo.wednet.edu>
2025-11-24 10:24:36 -0800

    magnets changable

--- Assets/Nicholas/MagnetPlatform.cs
+++ Assets/Nicholas/MagnetPlatform.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class MagnetPlatform : MonoBehaviour
 {
+    [SerializeField] SpriteRenderer sr;
+
     public int polarity;
     public float attractionForce;
+    private Color32 polarityColor;
+
+    public int distance1;
+    public int distance2;
+    public int distance3;
+    public int distance4;
     private List<Playermovement> attractedObjects = new List<Playermovement>();
 
     public void FixedUpdate()
@@ -18,29 +26,49 @@ public class MagnetPlatform : MonoBehaviour
                 Rigidbody2D rb = pm.rb;
                 Vector2 direction = (transform.position - rb.transform.position);
                 float distance = Mathf.Sqrt( Mathf.Pow(2,transform.position.x - rb.transform.position.x) + Mathf.Pow(2, transform.position.y - rb.transform.position.y));
-                if (distance >= 0 && distance <= 3) {
+                if (distance >= 0 && distance <= distance1) {
                     rb.AddForce(direction * attractionForce * 6 * pm.polarity);
                 }
-                else if (distance > 3 && distance <= 5)
+                else if (distance > distance1 && distance <= distance2)
                 {
                     rb.AddForce(direction * attractionForce * 4 * pm.polarity);
                 }
-                else if (distance > 5 && distance <= 7)
+                else if (distance > distance2 && distance <= distance3)
                 {
                     rb.AddForce(direction * attractionForce * 2 * pm.polarity);
                 }
-                else if (distance > 7 && distance <= 9)
+                else if (distance > distance3 && distance <= distance4)
                 {
                     rb.AddForce(direction * attractionForce * pm.polarity);
                 }
             }
         }
     }
+    public void Update()
+    {
+        if(polarity == 1)
+        {
+            attractionForce = 1;
+            polarityColor = new Color32(0, 0, 255, 255);
+        }
+        else if(polarity == -1)
+        {
+            attractionForce = -1;
+            polarityColor = new Color32(255, 0, 0, 255);
+        }
+        else
+        {
+            attractionForce = 0;
+            polarityColor = new Color32(255, 0, 255, 255);
+        }
+        sr.color = polarityColor;
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         Playermovement pm = collision.GetComponent<Playermovement>();
         if (pm != null && !attractedObjects.Contains(pm))
         {
+            pm.magnetised = true;
             attractedObjects.Add(pm);
         }
 
@@ -52,6 +80,7 @@ public class MagnetPlatform : MonoBehaviour
             Playermovement pm = collision.GetComponent<Playermovement>();
             if (pm != null)
             {
+                pm.magnetised = false;
                 attractedObjects.Remove(pm);
             }
         }
