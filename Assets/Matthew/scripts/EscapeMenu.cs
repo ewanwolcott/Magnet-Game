65cecd973327f441f99cb5978931f8b4e1e0c647
Dashing <bobby.bobbyjoejoejoe@gmail.com>
2025-12-02 22:07:43 -0800

    escape menu almost done just has some big ish bugs

---
+++ Assets/Matthew/scripts/EscapeMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+public class EscapeMenu : MonoBehaviour
+{
+    public GameObject escapemenu;
+    public GameObject escapemenubackground;
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Title_Screne");
+    }
+    public void Resume()
+    {
+        escapemenu.SetActive(false);
+        escapemenubackground.SetActive(false);
+        IsEscaped = false;
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    // has escape menu open
+    private bool IsEscaped = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && IsEscaped == false)
+        {
+            escapemenu.SetActive(true);
+            escapemenubackground.SetActive(true);
+            IsEscaped = true;
+            Time.timeScale = 0;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Escape) && IsEscaped == true)
+        {
+            escapemenu.SetActive(false);
+            escapemenubackground.SetActive(false);
+            IsEscaped = false;
+            Time.timeScale = 1;
+        }
+    }
+}
--- Assets/Matthew/scripts/MainMenu.cs
+++ Assets/Matthew/scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Test");
+        SceneManager.LoadScene("esc_ui");
     }
 
     public void PlayCredits()
