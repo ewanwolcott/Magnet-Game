34fd612050b671eae1cebed40ee5196f37cb217d
Arcturus <systriekglitchyt@gmail.com>
2026-07-05 17:34:16 -0700

    added music to all levels and fixed bug

---
+++ Assets/Nicholas/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSrc;
+    private static MusicManager instance;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Title_Screne")
+        {
+            audioSrc.Stop();
+        }
+        else
+        {
+            if(!audioSrc.isPlaying)
+                audioSrc.Play();
+        }
+    }
+}
